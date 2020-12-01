21728eb76b87da308be012c85f8bc2f4738cbd6c
Masi <72994325+MasiShaneshin@users.noreply.github.com>
2020-12-01 18:19:23 -0500

    Add files via upload
    Add setupInput and handleUserInput in play.js

--- play.js
+++ play.js
@@ -1,3 +1,27 @@
-const connect = require('./client');
-console.log("connecting...");
+const { connect } = require('./client');
+const { setupInput } = require('./input');
+
+console.log('Connecting ...');
 connect();
+
+const setupInput = function(conn) {
+  connection = conn;
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+
+  stdin.on('data',(key)=>{
+    if(key==='\u0003'){
+    
+      process.exit();
+    }
+    handleUserInput();
+    
+    });
+    return stdin;
+    }
+    module.exports={
+      handleUserInput,setupInput
+    
+    }
