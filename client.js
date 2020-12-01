bf389c7a504958f063879eea5638a253af78294f
Masi <72994325+MasiShaneshin@users.noreply.github.com>
2020-12-01 15:43:43 -0500

    Update client.js

--- client.js
+++ client.js
@@ -1,11 +1,33 @@
+const net = require('net');
 const connect = function() {
    
   const conn = net.createConnection({ 
-    host: 'localhost',
+    host: '192.168.0.22 ',
     port: 50541
   });
   // interpret incoming data as text
-  conn.setEncoding('utf8'); 
+conn.setEncoding('utf8'); 
+conn.on('connect',()=>{
+
+console.log("Successfully connected to game server");
+});
+
+conn.on('connect',()=>{
+conn.write("Name: MAS");
+});
+
+conn.on('connect',()=>{
+  conn.write("Move: up");
+  });
+  conn.on('connect',()=>{
+    conn.write("Move: right");
+    });
+
+    
+  conn.on('date',(data)=>{
+    console.log("Server says:,data");
+    });
 
   return conn;
 }
+module.exports=connect;
