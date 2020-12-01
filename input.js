c500d660038d4d00d97824e7dff825788ac69796
Masi <72994325+MasiShaneshin@users.noreply.github.com>
2020-12-01 18:29:21 -0500

    Update input.js

--- input.js
+++ input.js
@@ -1,24 +1,40 @@
 
-let connection; 
+let connection;
+
+const handleUserInput = function(key) {
+  if (key === "u") {
+      //conn.write("Move: up");
+    connection.write("Move: up");
+  }
+  if(key === "d") {
+    connection.write("Move: down");
+  }
+  if(key === "l") {
+    connection.write("Move: left");
+  }
+  if(key === "r") {
+    connection.write("Move: right");
+  }
+}
+
 const setupInput = function(conn) {
   connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
- 
 
-stdin.on('data',(key)=>{
-if(key==='\u0003'){
+  stdin.on('data', (key) => {
+    // \u0003 maps to ctrl+c input
+    if (key === '\u0003') {
+      process.exit();
+    }
+    handleUserInput(key);
+  });
 
-  process.exit();
+  return stdin;
 }
-handleUserInput();
-
-});
-return stdin;
-}
-module.exports={
-  handleUserInput,setupInput
-
+module.exports = {
+  setupInput,
+  handleUserInput
 }
