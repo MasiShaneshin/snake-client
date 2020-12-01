54564409dcb29357c7f1a6d2a99d7d1e68e37518
Masi <72994325+MasiShaneshin@users.noreply.github.com>
2020-12-01 18:24:16 -0500

    Add files via upload

---
+++ input.js
@@ -0,0 +1,24 @@
+
+let connection; 
+const setupInput = function(conn) {
+  connection = conn;
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+ 
+
+stdin.on('data',(key)=>{
+if(key==='\u0003'){
+
+  process.exit();
+}
+handleUserInput();
+
+});
+return stdin;
+}
+module.exports={
+  handleUserInput,setupInput
+
+}
