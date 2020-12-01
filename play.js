3bda42e56f1ea249acc1c7ddbfa64b3fbf428626
Masi <72994325+MasiShaneshin@users.noreply.github.com>
2020-12-01 18:25:01 -0500

    Update play.js

--- play.js
+++ play.js
@@ -4,24 +4,4 @@ const { setupInput } = require('./input');
 console.log('Connecting ...');
 connect();
 
-const setupInput = function(conn) {
-  connection = conn;
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-
-  stdin.on('data',(key)=>{
-    if(key==='\u0003'){
-    
-      process.exit();
-    }
-    handleUserInput();
-    
-    });
-    return stdin;
-    }
-    module.exports={
-      handleUserInput,setupInput
-    
-    }
+setupInput();
